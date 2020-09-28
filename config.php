45795081a4f3ecb9cae2e7e1de0bfd5a90378360
seruling <seruling@gmail.com>
2020-09-28 11:29:45 +0800

    Update config.php
    update username

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Asia/Kuala_Lumpur');
 error_reporting(E_ALL);
 define("SECURE", TRUE); 
 $host = "localhost";
-$db_username = "root";
+$db_username = "kedaigambar";
 $db_password = "MySQL@332";
 $database = "kedaigambar";
 $conn = mysqli_connect($host,$db_username,$db_password,$database);
