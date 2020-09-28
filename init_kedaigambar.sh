79644c000a456f9ed423cd54dc799e24eeeed3e6
seruling <seruling@gmail.com>
2020-09-28 12:20:28 +0800

    Create init_kedaigambar.sh

---
+++ init_kedaigambar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apt update
+apt install -y git apache2 mysql-server php libapache2-mod-php php-mysql php-gd php-zip php-cli
+echo 1 > /var/www/html/index.html
+git clone https://github.com/seruling/kedaigambar.git /var/www/html/kedaigambar
+chown -R ubuntu:ubuntu /var/www/html/kedaigambar/
+chmod 777 /var/www/html/kedaigambar/images/
+mysql -uroot -e "create database kedaigambar"
+mysql -uroot -e "CREATE USER 'kedaigambar'@'%' IDENTIFIED BY 'MySQL@332'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON kedaigambar.* TO 'kedaigambar'@'%'"
+mysql -uroot kedaigambar < /var/www/html/kedaigambar/kedaigambar.sql
+rm -rf /var/www/html/kedaigambar/kedaigambar.sql
+rm -rf /var/www/html/kedaigambar/init_kedaigambar.sh
