71fb9489a39eff9d9a93cf1cf74a9c1e94c895a1
seruling <seruling@gmail.com>
2020-09-28 12:15:22 +0800

    Update download.php

--- download.php
+++ download.php
@@ -1,6 +1,6 @@
 <?php
 $host = "localhost";
-$db_username = "ubuntu";
+$db_username = "kedaigambar";
 $db_password = "MySQL@332";
 $database = "kedaigambar";
 $conn = mysqli_connect($host,$db_username,$db_password,$database);
